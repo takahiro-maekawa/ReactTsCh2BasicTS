9e48ce8b8df0517fe08edc336efc0a24a5568aa3
takahiro-maekawa <koma4024@gmail.com>
2024-03-09 10:03:41 +0900

    型ガードを活用

--- src/functionSample/unknownFetch.ts
+++ src/functionSample/unknownFetch.ts
@@ -1,15 +1,51 @@
 export function unknownFetch() {
-    fetch("https://swapi.dev/api/people/1")
+    
+    // yugioh APIからデータを取得
+    fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Dark%20Magician")
     .then((response) => response.json())
     .then((data: unknown) => {
+        console.log(data);
+        // このif文中においては dataはdataというプロパティを持っているものとみなされる
+        if(containsData(data)){ 
+            return data.data;
+        }
+        return null;
+    })
+    .then((data: unknown) => {
+        console.log(data)
+        if (isList(data)){
+            return data[0];
+        }
+        return null;
+    }
+    )
+    .then((data: unknown) => {
+        console.log(data);
         if (isCharacter(data)){
             console.log("name", data.name);
-        }
+            return null;
+        };
+        console.log("Failed");
     });
 }
 
+function containsData( // 実際にdataを持っていれば、dataを持つオブジェクトとみなす
+    value: any
+    ):
+    value is {data: any}{
+        return "data" in value;
+    }
+
+function isList( // Array型と判定できれば、Array型とみなす
+    value: any
+    ):
+    value is Array<any> {
+        return Array.isArray(value); 
+    }
+
 function isCharacter(
     character: any
-):character is {name: string} {
-    return "name" in character;
-} 
+    ):
+    character is {name: string} {
+        return "name" in character; // characterがnameプロパティを持っているか
+    } 
