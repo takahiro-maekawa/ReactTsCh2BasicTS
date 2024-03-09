124381eea71c1b9b545f11a1b04da568baddc2db
takahiro-maekawa <koma4024@gmail.com>
2024-03-09 09:26:37 +0900

    fetchを使ったサンプルを準備

--- src/functionSample/index.js
+++ src/functionSample/index.js
@@ -1,4 +1,6 @@
 import {calculateTotalPrice} from './calculateTotalPrice.ts'
+import {unknownFetch} from './unknownFetch.ts'
+
 let firstName = "Fred";
 console.log("アイアムフレッド")
 
@@ -6,4 +8,7 @@ console.log("TypeScript Sample");
 const info = {product: {name:'apple', unitPrice: 50}, quantity: 30, discount: 0.2};
 
 console.log(calculateTotalPrice(info));
+
+console.log("fetch sample");
+unknownFetch();
 // ルートディレクトリでts-node --esm src/functionSample/index.jsで実行できます
